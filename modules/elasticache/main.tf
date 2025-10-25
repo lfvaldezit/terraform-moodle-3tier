3004592a5fafb10587eb78b98cc4f9625d595d31
unknown <lfvaldez.it@gmail.com>
2025-10-25 09:29:10 -0300

    Update elasticache module files

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -2,7 +2,7 @@ resource "aws_elasticache_cluster" "this" {
   cluster_id           = var.name
   engine               = "redis"
   node_type            = var.node_type
-  num_cache_nodes      = 2
+  num_cache_nodes      = 1
   parameter_group_name = "default.redis7"
   port                 = 6379
   security_group_ids = var.security_group_ids
@@ -10,8 +10,25 @@ resource "aws_elasticache_cluster" "this" {
   tags = merge({Name = var.name}, var.common_tags)
 }
 
-
 resource "aws_elasticache_subnet_group" "this" {
   name       = "${var.name}-sn-grp"
-  subnet_ids = var.subnet_ids
+  subnet_ids = var.subnet_ids                                                                  
 }
+
+# resource "aws_elasticache_replication_group" "this" {
+#   replication_group_id          = var.name
+#   description = "Redis"
+#   node_type                     = var.node_type
+#   num_cache_clusters = 2
+#   parameter_group_name          = "default.redis7"
+#   port                          = 6379
+#   automatic_failover_enabled    = true
+#   multi_az_enabled              = true
+#   subnet_group_name             = aws_elasticache_subnet_group.this.name
+#   security_group_ids            = var.security_group_ids
+#   engine_version                = "7.0"
+#   apply_immediately             = true
+#   at_rest_encryption_enabled    = false
+#   transit_encryption_enabled    = false
+#    tags = merge({Name = var.name}, var.common_tags)
+# }
