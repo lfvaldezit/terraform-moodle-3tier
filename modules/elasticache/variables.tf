9354836ac2d3232eb152399d5eacd95563efd1a9
unknown <lfvaldez.it@gmail.com>
2025-10-27 08:25:05 -0300

    Update elasticache module files

--- modules/elasticache/variables.tf
+++ modules/elasticache/variables.tf
@@ -1,19 +1,24 @@
 variable "name" {
+  description = "Base name prefix used to label all created resources "
     type = string
 }
 
 variable "node_type" {
+  description = "Instance class to be used"
   type = string
 }
 
 variable "security_group_ids" {
+  description = "Security group IDs for Elasticache"
   type = set(string)
 }
 
 variable "subnet_ids" {
+  description = "Set of VPC Subnet IDs for the cache subnet group"
   type = set(string)
 }
 
 variable "common_tags" {
+  description = "Common tags for all resources"
   type = map(string)
 }
