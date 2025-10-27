e3e900461ad0fd12834f3fa87fd997be2fb9b7a2
unknown <lfvaldez.it@gmail.com>
2025-10-27 08:42:07 -0300

    Update parameter-store module files

--- modules/parameter-store/variables.tf
+++ modules/parameter-store/variables.tf
@@ -1,11 +1,14 @@
 variable "param_name" {
+  description = "Name of the parameter"
   type = string
 }
 
 variable "type" {
+  description = "Type of the parameter"
     type = string 
 }
 
 variable "value" {
+  description = "Value of the parameter"
     type = string
 }
