0a5228682f959044ca2ced242ca801761274e7ab
unknown <lfvaldez.it@gmail.com>
2025-10-27 08:19:54 -0300

    Update EFS module files

--- modules/efs/variables.tf
+++ modules/efs/variables.tf
@@ -1,16 +1,20 @@
 variable "subnet_id" {
+  description = "Set of subnet IDs for mount target"
   type = list(string)
 }
 
 variable "name" {
+  description = "Base name prefix used to label all created resources "
   type = string
 }
 
 variable "security_groups" {
+  description = "Security group IDs for EFS"
   type = set(string)
 }
 
 variable "common_tags" {
+  description = "Common tags for all resources"
   type = map(string)
   default = {}
 }
